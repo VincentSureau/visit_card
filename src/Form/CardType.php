9381cf0c87db841e8bdc101c20ff2343834f06f7
Vincent Sureau <vincentsureau5@gmail.com>
2023-04-13 14:50:37 +0200

    updated card form labels

--- src/Form/CardType.php
+++ src/Form/CardType.php
@@ -4,23 +4,54 @@ namespace App\Form;
 
 use App\Entity\Card;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class CardType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('email')
-            ->add('phoneNumber')
-            ->add('address1')
-            ->add('address2')
-            ->add('postCode')
-            ->add('city')
-            ->add('civility')
+            ->add('civility', ChoiceType::class, [
+                'label' => 'Civilité',
+                'choices' => [
+                    'M.' => 'M.',
+                    'Mme.' => 'Mme.'
+                ],
+                'multiple' => false,
+                'expanded' => true,
+                'attr' => [
+                    'class' => 'd-flex',
+                ],
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email'
+            ])
+            ->add('phoneNumber', TelType::class, [
+                'label' => 'Téléphone'
+            ])
+            ->add('address1', TextType::class, [
+                'label' => 'Adresse'
+            ])
+            ->add('address2', TextType::class, [
+                'label' => 'Complément d\'adresse'
+            ])
+            ->add('postCode', TextType::class, [
+                'label' => 'Code postal'
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville'
+            ])
         ;
     }
 
